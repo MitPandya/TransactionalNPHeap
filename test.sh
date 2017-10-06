a32ee5e6ed1ce84df37dcda183525fad1450eb86
Rachit Shrivastava <rshriva@ncsu.edu>
2017-10-06 00:12:58 -0400

    New Script

--- test.sh
+++ test.sh
@@ -1,5 +1,21 @@
+sudo dmesg -C
+sudo rmmod tnpheap
+sudo rmmod npheap
+cd kernel_module
+sudo make clean
+make
+sudo make install
+cd ../library
+sudo make clean
+make
+sudo make install
+cd ../benchmark
+sudo make clean
+sudo make benchmark
+sudo make validate
+cd ..
 # change this to where your npheap is.
-sudo insmod ../NPHeap/kernel_module/npheap.ko
+sudo insmod NPHeap/kernel_module/npheap.ko
 sudo chmod 777 /dev/npheap
 sudo insmod kernel_module/tnpheap.ko
 sudo chmod 777 /dev/tnpheap
