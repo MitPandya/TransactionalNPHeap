2dd427f31151002f61fc78bd6f9c4eebcf47edaf
Rachit Shrivastava <rshriva@ncsu.edu>
2017-10-19 17:09:51 -0400

    npheap alloc

--- library/tnpheap.c
+++ library/tnpheap.c
@@ -13,7 +13,7 @@
 
 __u64 tnpheap_get_version(int npheap_dev, int tnpheap_dev, __u64 offset)
 {
-     return 0;
+    return 0;
 }
 
 
@@ -26,17 +26,18 @@ int tnpheap_handler(int sig, siginfo_t *si)
 
 void *tnpheap_alloc(int npheap_dev, int tnpheap_dev, __u64 offset, __u64 size)
 {
-    return NULL;     
+    __u64 aligned_size = ((size + getpagesize() - 1) / getpagesize())*getpagesize();
+    return mmap(0,aligned_size,PROT_READ|PROT_WRITE,MAP_SHARED,tnpheap_dev,offset*getpagesize());
 }
 
 __u64 tnpheap_start_tx(int npheap_dev, int tnpheap_dev)
 {
-	printk(KERN_INFO "in .c file");
-    return 0;
+	struct tnpheap_cmd cmd;
+	
+	return tnpheap_ioctl(tnpheap_dev, TNPHEAP_IOCTL_START_TX, &cmd);
 }
 
 int tnpheap_commit(int npheap_dev, int tnpheap_dev)
 {
     return 0;
 }
-
