0704086f3fb620ca54347aa7c978963e663c1ecf
Rachit Shrivastava <rshriva@ncsu.edu>
2017-10-22 17:44:57 -0400

    Creating Buffer

--- library/tnpheap.c
+++ library/tnpheap.c
@@ -11,6 +11,8 @@
 #include <malloc.h>
 #include <string.h>
 
+char* buffer;
+
 __u64 tnpheap_get_version(int npheap_dev, int tnpheap_dev, __u64 offset)
 {
     return 0;
@@ -27,13 +29,17 @@ int tnpheap_handler(int sig, siginfo_t *si)
 void *tnpheap_alloc(int npheap_dev, int tnpheap_dev, __u64 offset, __u64 size)
 {
     __u64 aligned_size = ((size + getpagesize() - 1) / getpagesize())*getpagesize();
+
+    //creating a buffer memory for transactions
+    buffer = (char*) mmap(0,aligned_size,PROT_READ|PROT_WRITE,MAP_SHARED,npheap_dev,offset*getpagesize());
+
     return mmap(0,aligned_size,PROT_READ|PROT_WRITE,MAP_SHARED,tnpheap_dev,offset*getpagesize());
 }
 
 __u64 tnpheap_start_tx(int npheap_dev, int tnpheap_dev)
 {
 	struct tnpheap_cmd cmd;
-	
+	cmd.offset = npheap_dev;
 	return tnpheap_ioctl(tnpheap_dev, TNPHEAP_IOCTL_START_TX, &cmd);
 }
 
